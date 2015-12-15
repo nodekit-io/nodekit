92fbfd97a7e74d7e777b06da580011d39772dc7d
guycreate <guycreate@users.noreply.github.com>
2015-12-15 16:30:13 -0600

    updates

--- src/nodekit/platform-ios/NKWKViewController.swift
+++ src/nodekit/platform-ios/NKWKViewController.swift
@@ -19,11 +19,11 @@
 import UIKit
 import Foundation
 import WebKit
- class NKWKViewController: UIViewController, WKScriptMessageHandler {
+ public class NKWKViewController: UIViewController, WKScriptMessageHandler {
     
     private var webView: WKWebView?
     
-    override  func loadView() {
+    override public func loadView() {
        let config = WKWebViewConfiguration()
        let webPrefs = WKPreferences();
         
@@ -77,7 +77,7 @@ import WebKit
         
     }
     
-    override func viewDidLoad() {
+    override public func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(netHex: 0xFFCE54)
@@ -85,20 +85,20 @@ import WebKit
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func didReceiveMemoryWarning() {
+    override public func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-   func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage)
+  public func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage)
     {
         print(message.description)
     }
     
 }
 
-extension UIColor {
-    convenience init(red: Int, green: Int, blue: Int) {
+public extension UIColor {
+    convenience public init(red: Int, green: Int, blue: Int) {
         assert(red >= 0 && red <= 255, "Invalid red component")
         assert(green >= 0 && green <= 255, "Invalid green component")
         assert(blue >= 0 && blue <= 255, "Invalid blue component")
@@ -106,7 +106,7 @@ extension UIColor {
         self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
     }
     
-    convenience init(netHex:Int) {
+    convenience public init(netHex:Int) {
         self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
     }
 }
