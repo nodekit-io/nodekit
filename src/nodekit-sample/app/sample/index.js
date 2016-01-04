94e5dc4368addd3d5ac5ea6f0edc69a598d249b3
guycreate <guycreate@users.noreply.github.com>
2016-01-04 12:53:09 -0600

    readjusted sample

--- src/nodekit-sample/app/sample/index.js
+++ src/nodekit-sample/app/sample/index.js
@@ -1,29 +1,5 @@
 var util = require('util');
 
-var levelup = require('levelup');
-var memdown = require('memdown');
-
-function MemDB (opts, fn) {
-  if (typeof opts == 'function') {
-    fn = opts;
-    opts = {};
-  }
-  if (typeof opts == 'string') opts = {};
-  opts = opts || {};
-  opts.db = function (l) { return new memdown(l) };
-  return levelup('', opts, fn);
-}
-var db = MemDB();
-db.put('name', 'Yuri Irsenovich Kim')
-db.put('dob', '16 February 1941')
-db.put('spouse', 'Kim Young-sook')
-db.put('occupation', 'Clown')
-db.on('open', function() {console.log('open')})
-db.readStream()
-  .on('data', console.log)
-  .on('close', function () { console.log('Show\'s over folks!') })
-  
-
 console.log("STARTING SAMPLE APPLICATION");
 
 var fs = require('fs');
